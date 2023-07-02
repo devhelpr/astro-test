d4b256b8fb9598f39b8b195ed544cbfe1ec204b0
Maikel van de Lisdonk <maikel@devhelpr.com>
2023-07-02 13:18:57 +0200

    tailwind and nx fix

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
-
+import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   outDir: '../../dist/apps/website',
+  integrations: [tailwind()],
 });
--- apps/website/tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: [
-    './public/**/*.html',
-    './src/**/*.{astro,md,js,jsx,svelte,ts,tsx,vue}',
-    '../../libs/**/*.{astro,md,js,jsx,svelte,ts,tsx,vue}',
-  ],
-  theme: {},
-};
